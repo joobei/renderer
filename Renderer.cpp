eba9572c7cc59b07a9bb8705113320feee1cf649
Nikolaos Katzakis <joobei@users.noreply.github.com>
2021-03-13 15:59:31 +0100

    fix hard-coded vertex data in shader

--- Renderer.cpp
+++ Renderer.cpp
@@ -577,8 +577,8 @@ private:
         vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
         
         vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-        vertexInputInfo.vertexBindingDescriptionCount = 0;
-        vertexInputInfo.vertexAttributeDescriptionCount = 0;
+        vertexInputInfo.vertexBindingDescriptionCount = 1;
+        //vertexInputInfo.vertexAttributeDescriptionCount = 1;
 
         VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
         inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
