6e912fb8523ae26f9bfcdf84f24df292e00bf942
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2021-03-14 17:44:37 +0100

    temp does not compile

---
+++ render_lib/rl_create_command_pool.cpp
@@ -0,0 +1,13 @@
+#include "rl_render_lib.h"
+
+void Renderer::createCommandPool() {
+    QueueFamilyIndices queueFamilyIndices = findQueueFamilies(physicalDevice);
+
+    VkCommandPoolCreateInfo poolInfo{};
+    poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    poolInfo.queueFamilyIndex = queueFamilyIndices.graphicsFamily.value();
+
+    if (vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create command pool!");
+    }
+}
