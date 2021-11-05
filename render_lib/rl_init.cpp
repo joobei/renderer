75e6c8b9a48b1b5b7121541e6df1477be12b7832
Nikolaos Katzakis <joobei@users.noreply.github.com>
2021-11-05 10:06:55 +0200

    removed general validation layer messages

--- render_lib/rl_init.cpp
+++ render_lib/rl_init.cpp
@@ -24,7 +24,7 @@ void Renderer::create_instance() {
 
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.pApplicationName = "Vulkan Ventures";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
