d7ef496f06765683fdc43f75e1c2d2a1d19fd524
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2022-10-21 13:44:18 +0300

    read mesh with cgltf - linker error

--- render_lib/rl_render_lib.h
+++ render_lib/rl_render_lib.h
@@ -14,6 +14,7 @@
 #include <set>
 #include <array>
 #include <signal.h>
+#include "cgltf/cgltf.h"
 
 #ifdef NDEBUG
 const bool enableValidationLayers = false;
@@ -170,11 +171,13 @@ private:
 
 	};
 
-	const std::vector<Vertex> vertices = {
-		{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-		{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-		{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
-	};
+std::vector<Vertex> vertices;
+//	= {
+//		{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+//		{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+//		{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+//	};
+
 };
 
 static std::vector<char> readFile(const std::string& filename) {
