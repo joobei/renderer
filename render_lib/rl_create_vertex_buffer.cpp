0d655837b5bd55042d9b65023713315aacba3719
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2022-10-21 13:44:18 +0300

    read mesh with cgltf - linker error

--- render_lib/rl_create_vertex_buffer.cpp
+++ render_lib/rl_create_vertex_buffer.cpp
@@ -1,31 +1,50 @@
+#define CGLTF_IMPLEMENTATION
 #include "rl_render_lib.h"
 
 void Renderer::create_vertex_buffer() {
     VkBufferCreateInfo bufferInfo{};
     bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+
+    //new CGLTF code to import blender mesh ============
+    cgltf_options options = {};
+    cgltf_data* susanne_file = NULL;
+
+    cgltf_result result = cgltf_parse_file(&options, "susanne.glb", &susanne_file);
+
+    if(result == cgltf_result_success) {
+        std::cout << susanne_file->meshes_count;
+    }
+    else
+    {
+        std::cerr << "cgltf load failed" << std::endl;
+    }
+    // end new code ======================================
+
+
     bufferInfo.size = sizeof(vertices[0]) * vertices.size();
-    
+
     bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
     bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-    
+
     if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
             throw std::runtime_error("failed to create vertex buffer!");
         }
-    
+
     VkMemoryRequirements memRequirements;
     vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);
-    
+
     VkMemoryAllocateInfo allocInfo{};
     allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
     allocInfo.allocationSize = memRequirements.size;
     allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
-    
+
     if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
         throw std::runtime_error("failed to allocate vertex buffer memory!");
     }
-    
+
     vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
-    
+
+
     void* data;
     vkMapMemory(device, vertexBufferMemory, 0, bufferInfo.size, 0, &data);
     memcpy(data, vertices.data(), (size_t) bufferInfo.size);
