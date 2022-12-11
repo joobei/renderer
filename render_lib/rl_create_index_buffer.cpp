1262575a828fb78cc9216daa0118f1ff56dff13a
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2022-12-11 10:49:25 +0200

    edited buffer number. was 1, now 0 (only one buffer in gltf)

--- render_lib/rl_create_index_buffer.cpp
+++ render_lib/rl_create_index_buffer.cpp
@@ -22,13 +22,13 @@ void Renderer::create_index_buffer() {
     allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
 
     if (vkAllocateMemory(device, &allocInfo, nullptr, &indexBufferMemory) != VK_SUCCESS) {
-        throw std::runtime_error("failed to allocate vertex buffer memory!");
+        throw std::runtime_error("failed to allocate index buffer memory!");
     }
 
     vkBindBufferMemory(device, indexBuffer, indexBufferMemory, 0);
 
     void* data;
     vkMapMemory(device, indexBufferMemory, 0, bufferInfo.size, 0, &data);
-    memcpy(data, susanne_mesh->buffers[1].data, bufferInfo.size);
+    memcpy(data, susanne_mesh->buffers[0].data, bufferInfo.size);
     vkUnmapMemory(device, indexBufferMemory);
 }
