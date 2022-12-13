06d9f928ca0b366d8c5e7f00352ad89a9271efcb
Nikolaos Katzakis <2638631+joobei@users.noreply.github.com>
2022-12-13 19:07:33 +0200

    randomized color per vertex

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -2,12 +2,17 @@
 #extension GL_ARB_separate_shader_objects : enable
 
 layout(location = 0) in vec2 inPosition;
-layout(location = 1) in vec3 inColor;
-
 layout(location = 0) out vec3 fragColor;
+
+
+float PHI = 1.61803398874989484820459;  // Golden Ratio   
+
+float gold_noise(in vec2 xy, in float seed){
+       return fract(tan(distance(xy*PHI, xy)*seed)*xy.x);
+}
 
 void main() {
     gl_Position = vec4(inPosition, 0.0, 1.0);
-    fragColor = inColor;
+    fragColor = vec3(gold_noise(inPosition,inPosition.x),gold_noise(inPosition,inPosition.x),gold_noise(inPosition,inPosition.x));
 }
 
